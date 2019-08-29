8ac9ce86385d91d7e7dee0f92bf1f27b0b00401e
Maich45 <54651174+Maich45@users.noreply.github.com>
2019-08-29 09:39:56 +0700

    Update safelink_utama.js

--- safelink_utama.js
+++ safelink_utama.js
@@ -21,11 +21,10 @@ if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
 var x=Math.floor((Math.random()*4)+ 1);
 var xxx=null;
-if(x=="1"){xxx="prototype-machining-rapid-understands_28.html"}
+iif(x=="1"){xxx="prototype-machining-rapid-understands_28.html"}
 if(x=="2"){xxx="what-r-th-ftur-of-clud-pbx-srv.html"}
 if(x=="3"){xxx="could-claims-challenge-current.html"}
-if(x=="4"){xxx="how-important-are-insurance-carrier.html"}
-output.value = "https://get.apnadesishow.me/2019/08/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="4"){xxx="how-important-are-insurance-carrier.html"}a_to_ve[i].href="https://get.apnadesishow.me/2019/08/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
